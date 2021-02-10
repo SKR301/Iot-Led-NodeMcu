90ced6725d1946790ce9be6d0fae30083a5027dd
Saurav KumaR <chunnu.bihat@gmail.com>
2021-02-10 12:28:19 +0530

    Made website
    website with pin authentication, not secure(don't care)

---
+++ Web/js/conn.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCDTDslt0g9w9Y3ARW0ptHuLGgqXYR3cKY",
+    authDomain: "d440led.firebaseapp.com",
+    databaseURL: "https://d440led.firebaseio.com",
+    projectId: "d440led",
+    storageBucket: "d440led.appspot.com",
+    messagingSenderId: "130849169371",
+    appId: "1:130849169371:web:7c405c6df2871b4443d1fe"
+  };
+
+ firebase.initializeApp(firebaseConfig);
